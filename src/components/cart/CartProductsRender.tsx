2689b6b302b2b1a6418b95303cf6ae906c36677d
MauR27 <antonio27mauricio27@gmail.com>
2024-03-07 21:40:09 +0100

    Go to add your favorite instrument cart products render

--- src/components/cart/CartProductsRender.tsx
+++ src/components/cart/CartProductsRender.tsx
@@ -36,45 +36,62 @@ const CartProductsRender: FC<TIsloading> = ({ isLoading, cart }) => {
   if (isLoading) return <LoadingSpinner />;
   return (
     <>
-      {cart.map((product: any) => (
-        <Flex key={product._id} borderBottom="1px solid" borderColor="gray.200">
-          <Flex flex={1}>
-            <Card
-              minH="100px"
-              overflow="hidden"
-              variant="unstyled"
-              size="sm"
-              direction={isLargerThan580 ? "row" : "column-reverse"}
-              gap={2}
+      {cart.length ? (
+        <>
+          {cart.map((product: any) => (
+            <Flex
+              key={product._id}
+              borderBottom="1px solid"
+              borderColor="gray.200"
             >
-              <CardHeader>
-                <Image
-                  alt={product.name}
-                  src={product.images[0]}
-                  maxW={["60px", "80px", "100px"]}
-                  maxH={["60px", "80px", "100px"]}
-                  objectFit="cover"
-                />
-              </CardHeader>
-              <CardBody>
-                <Heading fontSize={["8px", "10px", "12px"]} fontWeight="bold">
-                  {product.name}
-                </Heading>
-                <Text
-                  fontWeight="normal"
-                  mt={2}
-                  fontSize={["8px", "10px", "12px"]}
+              <Flex flex={1}>
+                <Card
+                  minH="100px"
+                  overflow="hidden"
+                  variant="unstyled"
+                  size="sm"
+                  direction={isLargerThan580 ? "row" : "column-reverse"}
+                  gap={2}
                 >
-                  {product.brand}
-                </Text>
-              </CardBody>
-            </Card>
-          </Flex>
-          <Flex flex={1} gap={2}>
-            <CartProductQuantityControl cart={product} />
-          </Flex>
+                  <CardHeader>
+                    <Image
+                      alt={product.name}
+                      src={product.images[0]}
+                      maxW={["60px", "80px", "100px"]}
+                      maxH={["60px", "80px", "100px"]}
+                      objectFit="cover"
+                    />
+                  </CardHeader>
+                  <CardBody>
+                    <Heading
+                      fontSize={["8px", "10px", "12px"]}
+                      fontWeight="bold"
+                    >
+                      {product.name}
+                    </Heading>
+                    <Text
+                      fontWeight="normal"
+                      mt={2}
+                      fontSize={["8px", "10px", "12px"]}
+                    >
+                      {product.brand}
+                    </Text>
+                  </CardBody>
+                </Card>
+              </Flex>
+              <Flex flex={1} gap={2}>
+                <CartProductQuantityControl cart={product} />
+              </Flex>
+            </Flex>
+          ))}
+        </>
+      ) : (
+        <Flex minH="calc(100vh - 24rem)" align="center" justify="center">
+          <Text fontSize={["20px", "22px", "24px"]} textAlign="center">
+            Go to add your favorite instrument
+          </Text>
         </Flex>
-      ))}
+      )}
     </>
   );
 };
