c5d378fb0c3d3687cfd5187001441cfd9ffb9a1f
MauR27 <antonio27mauricio27@gmail.com>
2024-03-08 14:47:03 +0100

    #

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,33 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-const handler = async (req: any) => {
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
-  console.log(req);
-  console.log(session);
+const handler = async (req: NextRequest) => {
+  try {
+    const session = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+    console.log(req);
+    console.log(session);
 
-  if (!session) {
-    return NextResponse.redirect(new URL("/", req.url));
+    if (!session) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    if (
+      req.nextUrl.pathname === "/admin/add-products" &&
+      session.email !== process.env.ADMIN_ROLE
+    ) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+    if (
+      req.nextUrl.pathname === "/pages/profile" &&
+      session.provider === "google"
+    )
+      return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.next();
+  } catch (error) {
+    if (error instanceof Error) throw new Error(error.message);
   }
-  if (
-    req.nextUrl.pathname === "/admin/add-products" &&
-    session.email !== process.env.ADMIN_ROLE
-  ) {
-    return NextResponse.redirect(new URL("/", req.url));
-  }
-  if (
-    req.nextUrl.pathname === "/pages/profile" &&
-    session.provider === "google"
-  )
-    return NextResponse.redirect(new URL("/", req.url));
-  return NextResponse.next();
 };
 export default handler;
 
